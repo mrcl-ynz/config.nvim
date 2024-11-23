1f945a40626add46458eaf5d0d20f3eb145e3dde
Marcelo Yáñez <marcelo.araya1@mail.udp.cl>
2024-11-23 20:50:36 -0300

    feat: set neovim options

---
+++ lua/config/init.lua
@@ -0,0 +1,9 @@
+return {
+    options = require("config.options"),
+
+    setup = function(self)
+        vim.iter(self.options):each(function(k, v)
+            vim.opt[k] = v
+        end)
+    end,
+}
